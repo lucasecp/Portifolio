6bb2ef5b67f6c7c8c2db3764d9bdb6607c2fd39d
lucas <lucasecp7@gmail.com>
2020-07-24 22:37:29 -0300

    files

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const port = process.env.PORT
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(helmet())
+
+app.post('/api/feedback',(req,res)=>{
+    const data = req.body
+    const smtpTransport = nodemailer.createTransport({
+        host: process.env.HOST,
+        port: 587,
+        auth:{
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+       })
+       const mailOptions ={
+           from: process.env.EMAIL,   
+           to:process.env.EMAILTO,
+           subject:`Mensagem de ${data.name}`,
+           text: 'Mensagem',
+           html: `<strong>Email: ${data.email}<br/><br/><br/><br/><br/><br/><div>${data.comments}</div><strong/>`
+       }
+       smtpTransport.sendMail(mailOptions)
+       .then(msg=> {
+          console.log(msg)
+          return res.send(msg)
+        })
+       .catch(error=> {
+         return res.send(error)
+       } )
+})
+
+app.listen(port)
